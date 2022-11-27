8fcb81908d2c47c911cf41205009322a6dfc74ac
harzarul <harzarul@yahoo.com>
2022-11-27 16:36:38 +0800

    added till client

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Button = () => {
   return (
-    <div>Button</div>
+    <>
+    <button type='button' className={`py-4 px-6 font-poppins font-medium text-[18px] text-primary bg-blue-gradient rounded-[10px] outline-none mt-10 capitalize`}>
+      Get started
+    </button>
+    </>
   )
 }
 
--- src/components/FeedbackCard.jsx
+++ src/components/FeedbackCard.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import React from 'react';
+import {quotes} from '../assets';
+import styles from '../style';
 
-const FeedbackCard = () => {
+const FeedbackCard = ({content,name,title,img}) => {
   return (
-    <div>FeedbackCard</div>
+    <>
+    <div className='flex justify-between flex-col px-10 py-12 rounded-[20px] max-w-[370px] md:mr-10 sm:mr-5 mr-0 my-5 feedback-card'>
+      <img src={quotes} alt="quote" className='w-[42.6px] h-[27.6px] object-contain' />
+      <p className={`${styles.paragraph} my-10`}>
+        {content}
+      </p>
+
+      <div className='flex flex-row'>
+        <img src={img} alt="image"  className='w-[48px] h-[48px] rounded-full mr-2'/>
+        <div>
+          <h1 className='font-poppins font-semibold text-[20px] leading -[32px] text-white'>
+            {name}
+          </h1>
+          <p className='font-poppins font-normal text-[16px] leading-[24px] text-dimWhite'>
+            {title}
+          </p>
+        </div>
+      </div>
+    </div>
+    </>
   )
 }
 
